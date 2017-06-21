c3b0568f62c8f1427f2262fd44f43cf2fa8ba239
James Tortorelli <james.tortorelli@metova.com>
2017-06-21 12:46:22 -0500

    Add Customer class

---
+++ src/jtortorelli/refactoring/example/Customer.java
@@ -0,0 +1,20 @@
+package jtortorelli.refactoring.example;
+
+import java.util.Vector;
+
+public class Customer {
+    private String name;
+    private Vector<Rental> rentals = new Vector<>();
+
+    public Customer(String name) {
+        this.name = name;
+    }
+
+    public void addRental(Rental rental) {
+        this.rentals.addElement(rental);
+    }
+
+    public String getName() {
+        return name;
+    }
+}
