98b025e7b5396853a7b7c9bb3025a7d5db24a49d
James Tortorelli <james.tortorelli@metova.com>
2017-06-21 14:38:41 -0500

    Move getFrequentRenterPoints implementation into Price class

--- src/jtortorelli/refactoring/example/Price.java
+++ src/jtortorelli/refactoring/example/Price.java
@@ -4,4 +4,12 @@ abstract public class Price {
     abstract public int getPriceCode();
 
     abstract public double getCharge(int daysRented);
+
+    public int getFrequentRenterPoints(int daysRented) {
+        if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1) {
+            return 2;
+        } else {
+            return 1;
+        }
+    }
 }
